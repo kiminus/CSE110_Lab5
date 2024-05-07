ea35e4ffe6c0d4dadde80545f17568ab33b0343e
zilin <quitebig1@gmail.com>
2024-05-07 01:07:16 -0700

    initial commit

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,34 @@
 // explore.js
 
-window.addEventListener('DOMContentLoaded', init);
-
+window.addEventListener("DOMContentLoaded", init);
+const voice_image = document.querySelector("#expose img");
+const selectors = document.getElementById("voice-select");
+const voices_entries = [
+  [
+    "assets/images/air-horn.png",
+    "assets/images/car-horn.png",
+    "assets/images/party-horn.png",
+  ],
+  ["air horn", "car horn", "party horn"],
+  [
+    "assets/audio/air-horn.mp3",
+    "assets/audio/car-horn.mp3",
+    "assets/audio/party-horn.mp3",
+  ],
+];
+//console.log(selectors);
 function init() {
-  // TODO
+  voice_image.src = "assets/images/no-image.png";
+  populateVoices(voices_entries[1]);
+}
+
+function populateVoices(names) {
+  for (var i = 0; i < 3; i++) {
+    const option = document.createElement("option");
+    option.value = names[i];
+    option.text = names[i];
+    selectors.appendChild(option);
+  }
+  console.log(selectors);
+  selectors.addEventListener("change", when_voice_selected);
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,34 @@
 // expose.js
 
-window.addEventListener('DOMContentLoaded', init);
-
+window.addEventListener("DOMContentLoaded", init);
+const horn_image = document.querySelector("#expose img");
+const selectors = document.getElementById("horn-select");
+const voices_entries = [
+  [
+    "assets/images/air-horn.png",
+    "assets/images/car-horn.png",
+    "assets/images/party-horn.png",
+  ],
+  ["air-horn", "car-horn", "party-horn"],
+  [
+    "assets/audio/air-horn.mp3",
+    "assets/audio/car-horn.mp3",
+    "assets/audio/party-horn.mp3",
+  ],
+];
+console.log(selectors);
 function init() {
-  // TODO
+  console.log(voices_entries[0][1]);
+  horn_image.src = voices_entries[0][1];
+  selectors.addEventListener("change", when_voice_selected);
+}
+function when_voice_selected() {
+  console.log(selectors.value);
+  for (var i = 0; i < 3; i++) {
+    if (voices_entries[1][i] == selectors.value) {
+      horn_image.src = voices_entries[0][i];
+      return;
+    }
+  }
+  //console.log(entry_index);
 }
